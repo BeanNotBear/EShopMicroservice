06e09e3a661cdfbfb787bf3cf49f562b82b58853
NghiNV <nghinv03@gmail.com>
2024-11-29 00:16:08 +0700

    save data to database

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -1,14 +1,11 @@
-﻿using BuildingBlocks.CQRS;
-using Catalog.API.Models;
-
-namespace Catalog.API.Products.CreateProduct
+﻿namespace Catalog.API.Products.CreateProduct
 {
 
 	public record CreateProductCommand(string Name, List<string> Categories, string Description, string ImageFile, decimal Price)
 		: ICommand<CreateProductResult>;
 	public record CreateProductResult(Guid Id);
 
-	internal class CreateProductCommandHandler
+	internal class CreateProductCommandHandler(IDocumentSession session)
 		: ICommandHandler<CreateProductCommand, CreateProductResult>
 	{
 		public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
@@ -26,10 +23,12 @@ namespace Catalog.API.Products.CreateProduct
 				Price = command.Price,
 			};
 
-			// TODO
 			// Save to database
+			session.Store(product);
+			await session.SaveChangesAsync();
+
 			// Return CreateProductResult result
-			return new CreateProductResult(Guid.NewGuid());
+			return new CreateProductResult(product.Id);
 		}
 	}
 }
