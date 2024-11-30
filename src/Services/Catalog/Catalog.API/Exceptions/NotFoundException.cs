5164a8e6bf2246df1b4e4a4d463d70ec2f2c3281
NghiNV <nghinv03@gmail.com>
2024-11-30 11:17:02 +0700

    Get product  by id

---
+++ src/Services/Catalog/Catalog.API/Exceptions/NotFoundException.cs
@@ -0,0 +1,9 @@
+﻿namespace Catalog.API.Exceptions
+{
+	public class NotFoundException : Exception
+	{
+		public NotFoundException(string? message) : base(message)
+		{
+		}
+	}
+}
---
+++ src/Services/Catalog/Catalog.API/Products/GetProductById/GetProductByIdHandler.cs
@@ -0,0 +1,23 @@
+﻿
+using Catalog.API.Exceptions;
+
+namespace Catalog.API.Products.GetProductById
+{
+	public record GetProductByIdQuery(Guid Id) : IQuery<GetProductByIdResult>;
+	public record GetProductByIdResult(Product Product);
+
+	internal class GetProductByIdHandler(IDocumentSession session)
+		: IQueryHandler<GetProductByIdQuery, GetProductByIdResult>
+	{
+		public async Task<GetProductByIdResult> Handle(GetProductByIdQuery query, CancellationToken cancellationToken)
+		{
+			var product = await session.LoadAsync<Product>(query.Id);
+			if (product == null)
+			{
+				throw new NotFoundException($"Can not found with id: {query.Id}");
+			}
+			var result = new GetProductByIdResult(product);
+			return result;
+		}
+	}
+}
